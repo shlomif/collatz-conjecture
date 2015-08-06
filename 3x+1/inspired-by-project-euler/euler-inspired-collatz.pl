a95e87185ff9014cdc43965f35a7b1cbe8523d34
Shlomi Fish <shlomif@shlomifish.org>
2015-08-06 22:44:59 +0300

    Generalise and enhance the script.

--- 3x+1/inspired-by-project-euler/euler-inspired-collatz.pl
+++ 3x+1/inspired-by-project-euler/euler-inspired-collatz.pl
@@ -35,47 +35,131 @@ sub base
     {
         return '';
     }
+    if ($n < 0)
+    {
+        return '-' . base(-$n , $b);
+    }
 
     return base(int($n / $b), $b) . ($n % $b);
 }
 
-my $wanted_seq = shift(@ARGV);
-
-my $has_seq = '';
-my $last_n = 0;
-N:
-for my $n (1 .. 1_000_000_000_000)
+sub find_wanted_seq
 {
-    my $seq = seq(Math::GMP->new($n));
+    my ($n, $wanted_seq) = @_;
 
-    # print "N=$n Seq=$seq\n";
+    my $has_seq = '';
+    my $last_n = 0;
+    N:
+    while (1)
+    {
+        my $seq = seq(Math::GMP->new($n));
 
-    my $l = min(length($seq), length($wanted_seq));
+        # print "N=$n Seq=$seq\n";
 
-    my $has_l = sub {
-        for my $i (0 .. $l-1)
-        {
-            if (substr($seq, $i, 1) ne substr($wanted_seq, $i, 1))
+        my $l = min(length($seq), length($wanted_seq));
+
+        my $has_l = sub {
+            for my $i (0 .. $l-1)
             {
-                return $i;
+                if (substr($seq, $i, 1) ne substr($wanted_seq, $i, 1))
+                {
+                    return $i;
+                }
+            }
+            return $l;
+        }->();
+
+        if ($has_l > length($has_seq))
+        {
+            my $d = ($n - $last_n);
+            # printf "Diff = 0b%s ; 0t%s\n", base($d, 2), base($d, 3);
+            $last_n = $n;
+            $has_seq = substr($seq, 0, $has_l);
+
+            if ($has_seq =~ /\A\Q$wanted_seq\E/)
+            {
+                return $n;
             }
         }
-        return $l;
-    }->();
-
-    if ($has_l > length($has_seq))
+    }
+    continue
     {
-        my $d = ($n - $last_n);
-        if ($last_n)
-        {
-            printf "Diff = 0b%s ; 0t%s\n", base($d, 2), base($d, 3);
-        }
-        $last_n = $n;
-        $has_seq = substr($seq, 0, $has_l);
+        $n++;
+    }
+}
 
-        if ($has_seq =~ /\A\Q$wanted_seq\E/)
+sub find_exact_seq
+{
+    my ($exact_seq) = @_;
+
+    my $cb;
+
+    $cb = sub {
+        my ($so_far, $ops) = @_;
+
+        if (! @$ops)
         {
-            last N;
+            return $so_far;
         }
+        elsif (shift(@$ops) eq 'd')
+        {
+            return $cb->(($so_far << 1), $ops);
+        }
+        else
+        {
+            my $x = ($so_far << 1)-1;
+            if ($x % 3 != 0)
+            {
+                die "None";
+            }
+            return $cb->(($x / 3), $ops);
+        }
+    };
+
+    my $ret;
+    eval {
+        $ret = $cb->(Math::GMP->new(1), [reverse split //, $exact_seq]);
+    };
+
+    if ($@)
+    {
+        return undef();
+    }
+    else
+    {
+        return $ret;
+    }
+}
+
+my $input_seq = shift(@ARGV);
+
+my $last_n = 1;
+for my $l (1 .. length($input_seq)-1)
+{
+    my $sub_seq = substr($input_seq, 0, $l);
+
+    my $exact_n = find_exact_seq($sub_seq);
+    my $u_n = find_wanted_seq($last_n, $sub_seq.'u');
+    my $d_n = find_wanted_seq($last_n, $sub_seq.'d');
+
+    print "{ l=$l }\n";
+    if (defined $exact_n)
+    {
+        print "D[exact] = 0b" . base($exact_n-$last_n, 2) . "\n";
+    }
+    else
+    {
+        print "D[exact] = NONE\n";
+    }
+    print "D[d] = 0b" . base($d_n-$last_n, 2) . "\n";
+    print "D[u] = 0b" . base($u_n-$last_n, 2) . "\n";
+
+    if (substr($input_seq, $l, 1) eq 'u')
+    {
+        $last_n = $u_n;
+    }
+    else
+    {
+        $last_n = $d_n;
     }
 }
