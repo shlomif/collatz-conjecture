71b554331e53b94307f5a60b12134b94382156d2
Shlomi Fish <shlomif@shlomifish.org>
2015-08-06 19:50:33 +0300

    Mostly working but does not halt on input "dddd".

--- 3x+1/inspired-by-project-euler/euler-inspired-collatz.pl
+++ 3x+1/inspired-by-project-euler/euler-inspired-collatz.pl
@@ -3,6 +3,10 @@
 use strict;
 use warnings;
 
+no warnings 'recursion';
+
+use List::Util qw(min);
+
 use Math::GMP;
 
 sub seq
@@ -23,4 +27,50 @@ sub seq
     }
 }
 
-print seq(shift(@ARGV)), "\n";
+sub base
+{
+    my ($n  , $b) = @_;
+
+    if (!$n)
+    {
+        return '';
+    }
+
+    return base(int($n / $b), $b) . ($n % $b);
+}
+
+my $wanted_seq = shift(@ARGV);
+
+my $has_seq = '';
+my $last_n = 0;
+N:
+for my $n (1 .. 1_000_000_000_000)
+{
+    my $seq = seq(Math::GMP->new($n));
+
+    my $l = min(length($seq), length($wanted_seq));
+
+    my $has_l = sub {
+        for my $i (1 .. $l)
+        {
+            if (substr($seq, $i, 1) ne substr($wanted_seq, $i, 1))
+            {
+                return $i-1;
+            }
+        }
+        return $l;
+    }->();
+
+    if ($has_l > length($has_seq))
+    {
+        my $d = ($n - $last_n);
+        printf "Diff = 0b%s ; 0t%s\n", base($d, 2), base($d, 3);
+        $last_n = $n;
+        $has_seq = $seq;
+
+        if ($has_seq =~ /\A\Q$wanted_seq\E/)
+        {
+            last N;
+        }
+    }
+}
